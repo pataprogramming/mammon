7f09f12471f53e3c9c480cca993b4cc8262858a6
Paul Snyder <paul@pataprogramming.com>
2013-11-20 17:16:34 -0500

    Initial sketch of mammon

---
+++ src/lazybot/plugins/mammon.clj
@@ -0,0 +1,224 @@
+;;; Written by Paul L. Snyder <paul@pataprogramming.com>
+;;; Some code borrowed from lazybot.plugins.karma
+;;; by Michael D. Ivey <ivey@gweezlebur.com>
+;;; Licensed under the EPL
+
+(ns lazybot.plugins.mammon
+  (:use [lazybot registry info]
+        [useful.map :only [keyed]]
+        [somnium.congomongo :only [fetch fetch-one insert! update! destroy!]])
+  (:require[clojure.string :as s])
+  (:import (java.util.concurrent Executors ScheduledExecutorService TimeUnit)))
+
+(defn oxford-list [s]
+  (case (count s)
+    0    ""
+    1    (str (first s))
+    2    (str (first s) " and " (second s))
+    (str (s/join ", " (butlast s)) " and " (last s))))
+
+(defn- key-attrs
+  ([nick server channel]
+     (let [nick  (.toLowerCase nick)]
+       (keyed [nick server channel])))
+  ([nick server channel stock]
+     (let [nick  (.toLowerCase nick)
+           stock (.toLowerCase stock)]
+       (keyed [nick server channel stock ]))))
+
+(defn- set-shares
+  [nick server channel stock shares]
+  (let [attrs (key-attrs nick server channel stock)]
+    (if (= shares 0)
+      (destroy! :shares attrs)
+      (update! :shares attrs (assoc attrs :shares shares)))))
+
+(defn- rand-price
+  []
+  (+ 1 (/ (rand-int 900) 100.0)))
+
+(defn- set-price
+  [nick server channel price]
+  (let [attrs (key-attrs nick server channel)]
+    (if (= price 0)
+      (destroy! :price attrs)
+      (update! :price attrs (assoc attrs :price price)))))
+
+(defn- get-price
+  [nick server channel]
+  (let [user-map (fetch-one :price
+                            :where (key-attrs nick server channel))
+        price    (get user-map :price 0)]
+    (if (= price 0)
+      (let [ipo-price (rand-price)]
+        (set-price nick server channel ipo-price)
+        ipo-price)
+      price)))
+
+(defn- get-ownership
+  [nick server channel stock]
+  (let [stock (.toLowerCase stock)
+        user-map (fetch :shares
+                        :where (keyed [server channel stock]))]
+    (println "got" user-map "for server" server "channel" channel
+             "stock" stock)
+    user-map))
+
+(defn- get-stocks
+  [nick server channel]
+  (let [user-map (fetch :shares
+                        :where (key-attrs nick server channel))]
+    user-map))
+
+(defn- get-shares
+  [nick server channel stock]
+  (let [user-map (fetch-one :shares
+                            :where (key-attrs nick server channel stock))]
+    (get user-map :shares 0)))
+
+(def limit (ref {}))
+
+(let [scheduler (Executors/newScheduledThreadPool 1)]
+  (defn schedule [^Runnable task]
+    (.schedule scheduler task
+               5 TimeUnit/MINUTES)))
+
+;; TODO: mongo has atomic inc/dec commands - we should use those
+(defn- change-shares
+  [stock new-shares {:keys [^String nick com bot channel] :as com-m}]
+  (let [new-shares (if (< new-shares 0) 0 new-shares)
+        [msg apply]
+        (dosync
+         (let [current (get-in @limit [nick stock])]
+           (cond
+            (.equalsIgnoreCase nick stock) ["Buying shares in yourself is insider trading."]
+            ;(= current 3) ["You'll have to wait before your next transaction."]
+            (= current new-shares) ["No shares were bought or sold.."]
+            :else [;(str (get-in @bot [:config :prefix-arrow]) new-shares)
+                   (str nick " now owns " new-shares " shares of " stock ".")
+                   (alter limit update-in [nick stock] (fnil inc 0))])))]
+    (when apply
+      (set-shares nick (:server @com) channel stock new-shares)
+      ;(schedule #(dosync (alter limit update-in [nick stock] dec)))
+      )
+    (send-message com-m msg)))
+
+(defn- fmt-share
+  [{:keys [stock shares channel]} server & fmt-price?]
+  (let [base (str shares " shares of " stock)]
+    (if fmt-price?
+      (let [price (get-price stock server channel)]
+        (str base " at " (fmt-currency price channel)))
+      base)))
+
+(defn- fmt-owner
+  [{:keys [nick shares]}]
+  (str nick " (" shares ")"))
+
+(defn shares-fn
+  "Create a plugin command function that applies f to the stock owned bythe user specified in args."
+  [f]
+  (fn [{:keys [nick com channel args] :as com-m}]
+    (let [stock (first args)
+          shares (get-shares nick (:server @com) channel stock)
+          new-shares (f shares)]
+      (change-shares stock new-shares com-m))))
+
+(defn fmt-currency
+  [amount channel]
+  (str "\u20b1" (format "%.2f" amount)))
+
+(defn total-value
+  [stocks]
+  (reduce +
+          (map #(* (:shares %)
+                   (get-price (:stock %) (:server %) (:channel %)))
+               stocks)))
+
+
+(def print-price
+  (fn [{:keys [nick com channel args] :as com-m}]
+    (let [stock (first args)]
+      (if stock
+        (send-message com-m
+                      (if-let [price (get-price stock (:server @com) channel)]
+                        (str stock " is currently priced at "
+                             (fmt-currency price channel) ".")
+                        (str stock " is not listed on the " channel " exchange.")))
+        (send-message com-m
+                      (str "No stock symbol specified."))))))
+
+
+(def print-shares
+  (fn [{:keys [nick com channel args] :as com-m}]
+    (let [stock (first args)]
+      (if stock
+        (send-message com-m
+                      (let [shares (get-shares nick (:server @com) channel stock)
+                            price  (get-price stock (:server @com) channel)]
+                        (if shares
+                          (str nick " owns " shares " shares of " stock
+                               " at " (fmt-currency price channel)
+                               " (" (fmt-currency (* shares price) channel) " total).")
+                          (str "I have no record for shares of " stock
+                               " owned by " nick "."))))
+        (print-portfolio com-m)))))
+
+(def print-portfolio
+  (fn [{:keys [nick com bot channel args] :as com-m}]
+    (let [server (:server @com)]
+      (send-message com-m
+                    (let [stocks (get-stocks nick server channel)]
+                      (if (> (count stocks) 0)
+                        (str nick " owns "  (oxford-list (map #(fmt-share % server true) stocks))
+                             " (totalling " (fmt-currency (total-value stocks) channel) ").")
+                        (str nick " does not own any shares.")))))))
+
+
+(def print-ownership
+  (fn [{:keys [nick com bot channel args] :as com-m}]
+    (let [stock (or (first args) nick)]
+      (send-message com-m
+                    (let [owners (get-ownership nick (:server @com) channel stock)]
+                      (if (> (count owners) 0)
+                        (str "The users who own shares in " stock " are "
+                             (oxford-list
+                              (map fmt-owner
+                                   (reverse (sort-by :shares owners))))
+                             ".")
+                        (str "No users own shares in " stock ".")))))))
+
+(def buy (shares-fn #(+ % 100)))
+(def sell (shares-fn #(- % 100)))
+
+(defplugin
+  (:hook :on-message
+         (fn [{:keys [message] :as com-m}]
+           (let [[_ direction stock]
+                 (re-find #"^\((buy|sell|portfolio|ownership) (.+)\)(\s*;.*)?$" message)]
+             (when stock
+               ((case direction
+                  "buy" (shares-fn buy)
+                  "sell" (shares-fn sell)
+                  "portfolio" print-shares
+                  "ownership" print-ownership)
+                (merge com-m {:args [stock]}))))))
+  (:cmd
+   "Checks the shares of a stock that you own."
+   #{"shares" "stock" "owned"} print-shares)
+  (:cmd
+   "Checks the current price of a stock."
+   #{"price"} print-price)
+  (:cmd
+   "Checks all of the stocks that you own."
+   #{"portfolio"} print-portfolio)
+  (:cmd
+   "Increases the karma of the person you specify."
+   #{"buy"} buy)
+  (:cmd
+   "Decreases the karma of the person you specify."
+   #{"sell"} sell)
+  (:cmd
+   "Shows the shares owned in the given nick."
+   #{"ownership" "owners" "owner" "own"} print-ownership)
+  (:indexes [[:server :channel :nick]]))
